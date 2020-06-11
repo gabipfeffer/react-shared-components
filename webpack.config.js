143dc19df39ef1d3ac44cce0ccd6afeb9b714ca9
Gabriel Pfeffer <gabriel.pfeffer@guidesmiths.com>
2020-06-11 12:26:47 +0200

    new patch release

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   externals: [nodeExternals()],
   output: {
     filename: "index.js",
-    path: path.resolve(__dirname, "lib"),
+    path: path.resolve(__dirname, "dist"),
     library: "",
     libraryTarget: "commonjs",
   },
